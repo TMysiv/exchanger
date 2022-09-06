7433f526959863f114697d8e9570840114e3ab16
TMysiv <tarasmysiv@gmail.com>
2022-09-06 19:48:01 +0300

    add currency changer

---
+++ src/app/exchanger-currency.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {ICurrencies} from "./interfaces/currencies.interface";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExchangerCurrencyService {
+
+  private url = 'https://api.exchangerate.host/latest';
+
+
+  constructor(private httpClient:HttpClient) { }
+
+  getALLCurrencies():Observable<any>{
+    return this.httpClient.get<ICurrencies>(this.url)
+  }
+
+  getCurrency(fromCurrency:string,toCurrency:string):Observable<any>{
+    return this.httpClient.get<ICurrencies>(this.url + `?base=${fromCurrency}&symbols=${toCurrency}`)
+  }
+
+  getUsdAndEuro(currency:string):Observable<any>{
+    return this.httpClient.get<any>(this.url + `?base=${currency}`)
+  }
+}
---
+++ src/app/interfaces/currencies.interface.ts
@@ -0,0 +1,10 @@
+export interface ICurrencies {
+  base:string,
+  date:string,
+  motd:{
+    msg:string,
+    url:string
+  },
+  rates:{},
+  success:boolean
+}
